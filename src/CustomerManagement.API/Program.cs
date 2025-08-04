117650fe1a4375d3faa3e57c707979b03b9dd2b1
Ramana Jhammisetty <ramana.personal.work@gmail.com>
2025-08-04 22:56:52 +0530

    fix: removed duplicate SwaggerGen at service configuration

--- src/CustomerManagement.API/Program.cs
+++ src/CustomerManagement.API/Program.cs
@@ -12,7 +12,6 @@ internal class Program
         var builder = WebApplication.CreateBuilder(args);
 
         builder.Services.AddControllers();
-        builder.Services.AddSwaggerGen();
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddApplication();
         builder.Services.AddInfrastructure(builder.Configuration);
